7154e6f4cc3293b9c1e8bb591827f8cfd3b682fe
Jason <jason@10layer.com>
2024-08-01 14:10:01 +0200

    Fix dirs

--- src/bin/vectorize.js
+++ src/bin/vectorize.js
@@ -1,15 +1,19 @@
-import { Extract } from "./extract.js";
-import { Clean } from "./clean.js";
-import { Chunk } from "./chunk.js";
-import { Embeddings } from "./embeddings.js";
-import { Save } from "./save.js";
+import { Extract } from "../vectorize/extract.js";
+import { Clean } from "../vectorize/clean.js";
+import { Chunk } from "../vectorize/chunk.js";
+import { Embeddings } from "../vectorize/embeddings.js";
+import { Save } from "../vectorize/save.js";
 
 async function vectorize() {
     console.time("Vectorize");
-    await Extract();
+    // await Extract();
+    console.log("Cleaning...");
     await Clean();
+    console.log("Chunking...");
     await Chunk();
+    console.log("Embedding...");
     await Embeddings();
+    console.log("Saving...");
     await Save();
     console.timeEnd("Vectorize");
 }
